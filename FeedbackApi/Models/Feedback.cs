b39e45c1f33b51c36a042e9964f2404f473eaf24
vierushkan <vierushkan@DESKTOP-C262OOS>
2024-02-13 05:08:16 +0200

    Add project files.

---
+++ FeedbackApi/Models/Feedback.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FeedbackApi.Models
+{
+    public class Feedback
+    {
+        [Key]
+        public int Id { get; set; }
+        public string FeedbackMessage { get; set; } = string.Empty;
+        public DateTime Created { get; set; }
+    }
+}
